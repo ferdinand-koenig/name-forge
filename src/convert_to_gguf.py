247778bf8d73c9c0203da36fe2d5a2a75796491f
ferdinand-koenig <65330418+ferdinand-koenig@users.noreply.github.com>
2025-09-07 10:45:31 +0200

    Change converter to point to Mistral model

--- src/convert_to_gguf.py
+++ src/convert_to_gguf.py
@@ -11,9 +11,11 @@ from transformers import AutoTokenizer
 # -----------------------------
 # Configuration (edit these)
 # -----------------------------
-BASE_MODEL = "meta-llama/Meta-Llama-3.1-8b"  # path or HF repo of base model
-LORA_ADAPTER_PATH = "./lora-llama3-domain/lora_adapter"  # LoRA adapter output
-MERGED_OUTPUT_DIR = "./llama3_8b_merged"  # merged HF model output
+BASE_MODEL = (
+    "mistralai/Mistral-7B-Instruct-v0.3"  # path or HF repo of base model
+)
+LORA_ADAPTER_PATH = "./lora-mistral-domain/lora_adapter"  # LoRA adapter output
+MERGED_OUTPUT_DIR = "./mistral_7B_merged"  # merged HF model output
 
 # Create output dir if not exists
 os.makedirs(MERGED_OUTPUT_DIR, exist_ok=True)
